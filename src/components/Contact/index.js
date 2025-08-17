ebe7eabcfbb9f17be2a9fd63469e8478caa4232f
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-08-17 20:45:10 +0530

    Fixed: Form submission

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -121,6 +121,7 @@ const ContactButton = styled.input`
   color: ${({ theme }) => theme.text_primary};
   font-size: 18px;
   font-weight: 600;
+  cursor: pointer;
 `;
 
 // ----------------- COMPONENT -----------------
