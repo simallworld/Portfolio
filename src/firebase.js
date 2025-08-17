b901ce3f833b6cfa27c944cd5ade156bc82c739c
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-08-17 20:29:59 +0530

    Fix firebase import path for Vercel build

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics, isSupported } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBbdx5eiA4WC3GLGR7FBc6DNlqFixtWyN4",
   authDomain: "portfolio-contact-b748c.firebaseapp.com",
   projectId: "portfolio-contact-b748c",
-  storageBucket: "portfolio-contact-b748c.appspot.com",
+  storageBucket: "portfolio-contact-b748c.firebasestorage.app",
   messagingSenderId: "217616985732",
   appId: "1:217616985732:web:a6743d6f75aad86cf6dcd1",
-  measurementId: "G-4FCQWH5GK4",
+  measurementId: "G-4FCQWH5GK4"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-let analytics;
-if (typeof window !== "undefined") {
-  isSupported().then((yes) => {
-    if (yes) analytics = getAnalytics(app);
-  });
-}
-
-export { app, db, analytics };
+export const db = getFirestore(app);
