da4cc54c9157239f2a87ae028e443afc6e8fde6a
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-08-17 19:54:17 +0530

    Form connected with backend firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBbdx5eiA4WC3GLGR7FBc6DNlqFixtWyN4",
+  authDomain: "portfolio-contact-b748c.firebaseapp.com",
+  projectId: "portfolio-contact-b748c",
+  storageBucket: "portfolio-contact-b748c.firebasestorage.app",
+  messagingSenderId: "217616985732",
+  appId: "1:217616985732:web:a6743d6f75aad86cf6dcd1",
+  measurementId: "G-4FCQWH5GK4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
